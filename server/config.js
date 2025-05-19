191c062c4d2bfae5299b16014609d2c70662ea87
Farkas Bálint <135427982+Bubuka10@users.noreply.github.com>
2025-05-19 15:00:20 +0200

    Delete server/config.js

--- server/config.js
+++
@@ -1,5 +0,0 @@
-export const PORT = 5000;
-
-export const MONGO_URI = 'mongodb+srv://admin:admin@quizmaster.6z4vu.mongodb.net/quizMaster?retryWrites=true&w=majority&appName=QuizMaster';
-
-export const JWT_SECRET = 'secret123';
